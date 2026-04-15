646db212b5325243301e40d24481919af2d4705a
danush4756-ai <danush4756@gmail.com>
2026-04-15 06:41:47 +0530

    Priority Setting

--- src/test/java/com/companyname/projectname/ex_02_RestAssured_Basics/API_testing_multiple_test_cases.java
+++ src/test/java/com/companyname/projectname/ex_02_RestAssured_Basics/API_testing_multiple_test_cases.java
@@ -4,7 +4,7 @@ import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 
 public class API_testing_multiple_test_cases {
-    @Test
+    @Test(priority = 3)
     public void test_Positive_tc1(){
         String pincode = "110048";
         RestAssured
@@ -16,7 +16,7 @@ public class API_testing_multiple_test_cases {
                 .then()
                 .log().all().statusCode(200);
     }
-    @Test
+    @Test(priority = 1) //if Priority is -n it will have the highest priority.
     public void test_Negative_tc1(){
         String pincode = "@";
         RestAssured
