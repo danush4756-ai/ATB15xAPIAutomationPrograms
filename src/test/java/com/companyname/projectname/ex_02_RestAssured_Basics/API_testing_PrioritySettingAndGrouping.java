16afccc0174e735b69fe400b3f99c9516ce0e0c6
danush4756-ai <danush4756@gmail.com>
2026-04-15 09:00:01 +0530

    Priority Setting and grouping

--- src/test/java/com/companyname/projectname/ex_02_RestAssured_Basics/API_testing_PrioritySettingAndGrouping.java
+++ src/test/java/com/companyname/projectname/ex_02_RestAssured_Basics/API_testing_PrioritySettingAndGrouping.java
@@ -3,8 +3,10 @@ package com.companyname.projectname.ex_02_RestAssured_Basics;
 import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 
-public class API_testing_multiple_test_cases {
-    @Test(priority = 3)
+public class API_testing_PrioritySettingAndGrouping {
+    @Test(priority = 3, groups = {"pincode","positive"})/*We can group according to our needs but should
+    follow industry practice while doing it in reality. Like, we may group regression testing, smoke
+    testing, etc.*/
     public void test_Positive_tc1(){
         String pincode = "110048";
         RestAssured
@@ -16,7 +18,7 @@ public class API_testing_multiple_test_cases {
                 .then()
                 .log().all().statusCode(200);
     }
-    @Test(priority = 1) //if Priority is -n it will have the highest priority.
+    @Test(priority = 1, groups = {"pincode","negative"}) //if Priority is -n it will have the highest priority.
     public void test_Negative_tc1(){
         String pincode = "@";
         RestAssured
@@ -28,7 +30,7 @@ public class API_testing_multiple_test_cases {
                 .then()
                 .log().all().statusCode(200);
     }
-    @Test
+    @Test (groups = {"pincode","negative"})
     public void test_Negative_tc2(){
         String pincode = " ";
         RestAssured
