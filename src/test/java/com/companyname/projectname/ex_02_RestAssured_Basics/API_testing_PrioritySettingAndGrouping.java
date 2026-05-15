c863aa2bed524739ffb3fa58075548f530392233
DARSHAN <danush4756@gmail.com>
2026-05-15 07:39:26 +0530

    Grouping and setting priority for test cases

--- src/test/java/com/companyname/projectname/ex_02_RestAssured_Basics/API_testing_PrioritySettingAndGrouping.java
+++ src/test/java/com/companyname/projectname/ex_02_RestAssured_Basics/API_testing_PrioritySettingAndGrouping.java
@@ -4,39 +4,39 @@ import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 
 public class API_testing_PrioritySettingAndGrouping {
-    @Test(priority = 3, groups = {"pincode","positive"})/*We can group according to our needs but should
+    @Test(priority = 3, groups = {"PinCode","positive"})/*We can group according to our needs but should
     follow industry practice while doing it in reality. Like, we may group regression testing, smoke
     testing, etc.*/
     public void test_Positive_tc1(){
-        String pincode = "110048";
+        String PinCode = "110048";
         RestAssured
                 .given()
                 .baseUri("https://api.zippopotam.us")
-                .basePath("/IN/"+pincode)
+                .basePath("/IN/"+PinCode)
                 .when()
                 .get()
                 .then()
                 .log().all().statusCode(200);
     }
-    @Test(priority = 1, groups = {"pincode","negative"}) //if Priority is -n it will have the highest priority.
+    @Test(priority = 1, groups = {"PinCode","negative"}) //if Priority is -n it will have the highest priority.
     public void test_Negative_tc1(){
-        String pincode = "@";
+        String PinCode = "@";
         RestAssured
                 .given()
                 .baseUri("https://api.zippopotam.us")
-                .basePath("/IN/"+pincode)
+                .basePath("/IN/"+PinCode)
                 .when()
                 .get()
                 .then()
                 .log().all().statusCode(200);
     }
-    @Test (groups = {"pincode","negative"})
+    @Test (groups = {"PinCode","negative"})
     public void test_Negative_tc2(){
-        String pincode = " ";
+        String PinCode = " ";
         RestAssured
                 .given()
                 .baseUri("https://api.zippopotam.us")
-                .basePath("/IN/"+pincode)
+                .basePath("/IN/"+PinCode)
                 .when()
                 .get()
                 .then()
